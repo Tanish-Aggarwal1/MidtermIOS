52b1058e8aa7e4a587f083a0f53fce11a982ef24
Tanish Aggarwal <atanish6@gmail.com>
2026-06-15 09:23:36 -0400

    Initial Commit

---
+++ StudentActivityTracker_Aggatani/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StudentActivityTracker_Aggatani
+//
+//  Created by Tanish Aggarwal on 2026-06-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
